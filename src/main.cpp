680c96948fca8acaec9aebc92f3cb74d982fa187
rncbc <rncbc@5e98d84e-b994-457e-aaaa-8ae0a2fdc4f7>
2009-09-12 12:18:19 +0000

    - Automatic crash-dump reports, debugger stack-traces (gdb), back-   traces, whatever, are being introduced as a brand new configure   option (--enable-stacktrace) and default enabled on debug build   targets (--enable-debug).
    git-svn-id: http://svn.code.sf.net/p/qjackctl/code_attic/trunk@544 5e98d84e-b994-457e-aaaa-8ae0a2fdc4f7

--- src/main.cpp
+++ src/main.cpp
@@ -242,11 +242,75 @@ private:
 
 
 //-------------------------------------------------------------------------
+// stacktrace - Signal crash handler.
+//
+
+#ifdef CONFIG_STACKTRACE
+#if defined(__GNUC__) && defined(Q_OS_LINUX)
+
+#include <errno.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+void stacktrace ( int signo )
+{
+	pid_t pid;
+	int rc;
+	int status = 0;
+	char cmd[80];
+
+	// Reinstall default handler; prevent race conditions...
+	signal(signo, SIG_DFL);
+
+	static const char *shell  = "/bin/sh";
+	static const char *format = "gdb -q --batch --pid=%d --eval-command=bt";
+
+	snprintf(cmd, sizeof(cmd), format, (int) getpid());
+
+	pid = fork();
+
+	// Fork failure!
+	if (pid < 0)
+		return;
+
+	// Fork child...
+	if (pid == 0) {
+		execl(shell, shell, "-c", cmd, NULL);
+		_exit(1);
+		return;
+	}
+
+	// Parent here: wait for child to terminate...
+	do { rc = waitpid(pid, &status, 0); }
+	while ((rc < 0) && (errno == EINTR));
+
+	// Dispatch any logging, if any...
+	QApplication::processEvents(QEventLoop::AllEvents, 3000);
+
+	// Make sure everyone terminates...
+	kill(pid, SIGTERM);
+	_exit(1);
+}
+
+#endif
+#endif
+
+
+//-------------------------------------------------------------------------
 // main - The main program trunk.
 //
 
 int main ( int argc, char **argv )
 {
+#ifdef CONFIG_STACKTRACE
+#if defined(__GNUC__) && defined(Q_OS_LINUX)
+	signal(SIGILL,  stacktrace);
+	signal(SIGFPE,  stacktrace);
+	signal(SIGSEGV, stacktrace);
+	signal(SIGABRT, stacktrace);
+	signal(SIGBUS,  stacktrace);
+#endif
+#endif
 	qjackctlApplication app(argc, argv);
 
 	// Construct default settings; override with command line arguments.
