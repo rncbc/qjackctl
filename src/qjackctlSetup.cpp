4a49ffba901d5d22a05538d620a28493282de3ce
Kjetil Matheussen <k.s.matheussen@notam02.no>
2015-07-04 21:40:44 +0200

    WIN32: Use the portaudio driver by default (not alsa)

--- src/qjackctlSetup.cpp
+++ src/qjackctlSetup.cpp
@@ -371,7 +371,11 @@ bool qjackctlSetup::loadPreset ( qjackctlPreset& preset, const QString& sPreset 
 	preset.iWordLength  = m_settings.value("/WordLength", 16).toInt();
 	preset.iWait        = m_settings.value("/Wait", 21333).toInt();
 	preset.iChan        = m_settings.value("/Chan", 0).toInt();
+#ifdef WIN32
+	preset.sDriver      = m_settings.value("/Driver", "portaudio").toString();
+#else
 	preset.sDriver      = m_settings.value("/Driver", "alsa").toString();
+#endif
 	preset.sInterface   = m_settings.value("/Interface").toString();
 	preset.iAudio       = m_settings.value("/Audio", 0).toInt();
 	preset.iDither      = m_settings.value("/Dither", 0).toInt();
