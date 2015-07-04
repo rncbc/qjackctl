348e214632a4bbe03db6fbe68a14e1e380130c96
Kjetil Matheussen <k.s.matheussen@notam02.no>
2015-07-04 21:03:16 +0200

    Use latest portaudio

---
+++ mingw32/portaudio/CMakeFiles/portaudio_static.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/common/pa_allocation.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/common/pa_allocation.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/common/pa_converters.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/common/pa_converters.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/common/pa_cpuload.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/common/pa_cpuload.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/common/pa_debugprint.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/common/pa_debugprint.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/common/pa_dither.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/common/pa_dither.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/common/pa_front.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/common/pa_front.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/common/pa_process.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/common/pa_process.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/common/pa_ringbuffer.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/common/pa_ringbuffer.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/common/pa_stream.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/common/pa_stream.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/common/pa_trace.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/common/pa_trace.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/hostapi/skeleton/pa_hostapi_skeleton.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/hostapi/skeleton/pa_hostapi_skeleton.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/hostapi/wasapi/pa_win_wasapi.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/hostapi/wasapi/pa_win_wasapi.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/hostapi/wdmks/pa_win_wdmks.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/hostapi/wdmks/pa_win_wdmks.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/hostapi/wmme/pa_win_wmme.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/hostapi/wmme/pa_win_wmme.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/os/win/pa_win_coinitialize.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/os/win/pa_win_coinitialize.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/os/win/pa_win_hostapis.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/os/win/pa_win_hostapis.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/os/win/pa_win_util.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/os/win/pa_win_util.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/os/win/pa_win_waveformat.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/os/win/pa_win_waveformat.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/os/win/pa_win_wdmks_utils.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/os/win/pa_win_wdmks_utils.c.obj"
+  "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/src/os/win/pa_x86_plain_converters.c" "/home/kjetil/qjackctl/temp/qjackctl/mingw32/portaudio/CMakeFiles/portaudio_static.dir/src/os/win/pa_x86_plain_converters.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "PAWIN_USE_WDMKS_DEVICE_INFO"
+  "PORTAUDIO_CMAKE_GENERATED"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "src/os/win"
+  "include"
+  "src/common"
+  "."
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
