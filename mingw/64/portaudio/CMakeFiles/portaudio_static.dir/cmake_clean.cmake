3626b7875cce7a8f4fce95abcbbcca97f8153d64
Kjetil Matheussen <k.s.matheussen@notam02.no>
2015-07-05 11:33:34 +0200

    mingw: Add 32 and 64 bit builds of portaudio

---
+++ mingw/64/portaudio/CMakeFiles/portaudio_static.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/portaudio_static.dir/src/common/pa_allocation.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/common/pa_converters.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/common/pa_cpuload.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/common/pa_debugprint.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/common/pa_dither.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/common/pa_front.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/common/pa_process.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/common/pa_ringbuffer.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/common/pa_stream.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/common/pa_trace.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/hostapi/wmme/pa_win_wmme.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/hostapi/wasapi/pa_win_wasapi.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/hostapi/wdmks/pa_win_wdmks.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/hostapi/skeleton/pa_hostapi_skeleton.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/os/win/pa_win_hostapis.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/os/win/pa_win_util.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/os/win/pa_win_waveformat.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/os/win/pa_win_wdmks_utils.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/os/win/pa_win_coinitialize.c.obj"
+  "CMakeFiles/portaudio_static.dir/src/os/win/pa_x86_plain_converters.c.obj"
+  "libportaudio_static_x86.pdb"
+  "libportaudio_static_x86.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/portaudio_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
