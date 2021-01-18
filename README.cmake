44e8ed4da4bf8d7c452fb13b13b773bfff659a37
rncbc <rncbc@rncbc.org>
2021-01-18 09:47:07 +0000

    - CMake build system update (cmake >= 3.10)

--- README.cmake
+++ README.cmake
@@ -22,7 +22,7 @@ Linux and other systems at:
 How to use it?
 ==============
 
-1. You need CMake 3.1 or newer to build QjackCtl
+1. You need CMake 3.10 or newer to build QjackCtl
 
 2. Unpack the QjackCtl sources somewhere, or checkout the repository, 
    and create a build directory. For instance, using a command line shell:
