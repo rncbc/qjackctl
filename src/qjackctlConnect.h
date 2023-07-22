rncbc/qjackctl

commit 9b55062a680669f177127cd4e33a26957fa37eb7

- Connections: Fixed an old JACK client/ports aliases malfunction,   hopefully. (cf. GH issue #196) (EXPERIMENTAL)

src/qjackctlConnect.h
@@ -1,7 +1,7 @@
 // qjackctlConnect.h
 //
 /****************************************************************************
-   Copyright (C) 2003-2022, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2003-2023, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -99,8 +99,6 @@ public:
 	// - Natural decimal sorting comparator.
 	bool operator< (const QTreeWidgetItem& other) const;
 
-protected:
-
 	// Port name display name accessors.
 	void setPortText(const QString& sPortText, bool bRenameEnabled);
 	QString portText() const;
@@ -173,8 +171,6 @@ public:
 	// Proxy sort override method.
 	// - Natural decimal sorting comparator.
 	bool operator< (const QTreeWidgetItem& other) const;
-
-protected:
 
 	// Client name display name accessors.
 	void setClientText(const QString& sClientText, bool bRenameEnabled);

src/qjackctlJackConnect.cpp
@@ -1,7 +1,7 @@
 // qjackctlJackConnect.cpp
 //
 /****************************************************************************
-   Copyright (C) 2003-2019, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2003-2023, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -115,12 +115,38 @@ qjackctlJackPort::~qjackctlJackPort (void)
 // Pretty/display name method (virtual override).
 void qjackctlJackPort::updatePortName ( bool bRename )
 {
-#ifdef CONFIG_JACK_METADATA
 	jack_client_t *pJackClient = nullptr;
 	qjackctlMainForm *pMainForm = qjackctlMainForm::getInstance();
 	if (pMainForm)
 		pJackClient = pMainForm->jackClient();
-	if (pJackClient && qjackctlJackClientList::isJackClientPortMetadata()) {
+	if (pJackClient == nullptr)
+		return;
+
+#ifdef CONFIG_JACK_PORT_ALIASES
+	const int iJackClientPortAlias
+		= qjackctlJackClientList::jackClientPortAlias();
+	if (iJackClientPortAlias > 0) {
+		char *aliases[2];
+		const unsigned short alias_size = jack_port_name_size() + 1;
+		aliases[0] = new char [alias_size];
+		aliases[1] = new char [alias_size];
+		QString sPortName = portName();
+		QString sClientPort = clientName() + ':' + sPortName;
+		const QByteArray aClientPort = sClientPort.toUtf8();
+		const char *pszClientPort = aClientPort.constData();
+		jack_port_t *pJackPort = jack_port_by_name(pJackClient, pszClientPort);
+		if (pJackPort && jack_port_get_aliases(pJackPort, aliases) >= iJackClientPortAlias) {
+			sClientPort = QString::fromUtf8(aliases[iJackClientPortAlias - 1]);
+			sPortName = sClientPort.section(':', 1);
+		}
+		delete [] aliases[0];
+		delete [] aliases[1];
+		setPortText(sPortName, false);
+	}
+	else
+#endif
+#ifdef CONFIG_JACK_METADATA
+	if (qjackctlJackClientList::isJackClientPortMetadata()) {
 		bool bRenameEnabled = false;
 		QString sPortNameEx = portNameAlias(&bRenameEnabled);
 		const QString& sPortName = portName();
@@ -144,10 +170,10 @@ void qjackctlJackPort::updatePortName ( bool bRename )
 				&& !sPrettyName.isEmpty() && bRename) {
 				removePrettyName(pJackClient, port_uuid);
 			}
-			setPortText(sPortNameEx, bRenameEnabled);
-			return;
 		}
+		setPortText(sPortNameEx, bRenameEnabled);
 	}
+	else
 #endif
 	qjackctlPortItem::updatePortName(bRename);
 }
@@ -231,12 +257,15 @@ qjackctlJackPort *qjackctlJackClient::findJackPort ( const QString& sPortName )
 // Pretty/display name method (virtual override).
 void qjackctlJackClient::updateClientName ( bool bRename )
 {
-#ifdef CONFIG_JACK_METADATA
 	jack_client_t *pJackClient = nullptr;
 	qjackctlMainForm *pMainForm = qjackctlMainForm::getInstance();
 	if (pMainForm)
 		pJackClient = pMainForm->jackClient();
-	if (pJackClient && qjackctlJackClientList::isJackClientPortMetadata()) {
+	if (pJackClient == nullptr)
+		return;
+
+#ifdef CONFIG_JACK_METADATA
+	if (qjackctlJackClientList::isJackClientPortMetadata()) {
 		bool bRenameEnabled = false;
 		QString sClientNameEx = clientNameAlias(&bRenameEnabled);
 		const QString& sClientName = clientName();
@@ -337,12 +366,14 @@ int qjackctlJackClientList::updateClientPorts (void)
 		pszJackType = JACK_DEFAULT_MIDI_TYPE;
 #endif
 
+#ifdef CONFIG_JACK_PORT_ALIASES
 	char *aliases[2];
 	if (g_iJackClientPortAlias > 0) {
 		const unsigned short alias_size = jack_port_name_size() + 1;
 		aliases[0] = new char [alias_size];
 		aliases[1] = new char [alias_size];
 	}
+#endif
 
 	int iDirtyCount = 0;
 
@@ -355,30 +386,44 @@ int qjackctlJackClientList::updateClientPorts (void)
 			QString sClientPort = QString::fromUtf8(ppszClientPorts[iClientPort]);
 			qjackctlJackClient *pClient = nullptr;
 			qjackctlJackPort *pPort = nullptr;
-			jack_port_t *pJackPort = jack_port_by_name(pJackClient,
-				ppszClientPorts[iClientPort]);
-		#ifdef CONFIG_JACK_PORT_ALIASES
-			if (g_iJackClientPortAlias > 0 &&
-				jack_port_get_aliases(pJackPort, aliases) >= g_iJackClientPortAlias)
-				sClientPort = QString::fromUtf8(aliases[g_iJackClientPortAlias - 1]);
-		#endif
-			const int iColon = sClientPort.indexOf(':');
+			const char *pszClientPort = ppszClientPorts[iClientPort];
+			jack_port_t *pJackPort = jack_port_by_name(pJackClient, pszClientPort);
+			int iColon = sClientPort.indexOf(':');
 			if (pJackPort && iColon >= 0) {
 				QString sClientName = sClientPort.left(iColon);
 				QString sPortName = sClientPort.right(sClientPort.length() - iColon - 1);
 				pClient = static_cast<qjackctlJackClient *> (findClient(sClientName));
 				if (pClient)
 					pPort = static_cast<qjackctlJackPort *> (pClient->findPort(sPortName));
-				if (pClient == 0) {
+				if (pClient == nullptr) {
 					pClient = new qjackctlJackClient(this);
 					pClient->setClientName(sClientName);
-					iDirtyCount++;
+					++iDirtyCount;
 				}
-				if (pClient && pPort == 0) {
+				if (pClient && pPort == nullptr) {
 					pPort = new qjackctlJackPort(pClient, jack_port_flags(pJackPort));
 					pPort->setPortName(sPortName);
-					iDirtyCount++;
+					++iDirtyCount;
 				}
+			#ifdef CONFIG_JACK_PORT_ALIASES
+				if (g_iJackClientPortAlias > 0 &&
+					jack_port_get_aliases(pJackPort, aliases) >= g_iJackClientPortAlias) {
+					sClientPort = QString::fromUtf8(aliases[g_iJackClientPortAlias - 1]);
+					iColon = sClientPort.indexOf(':');
+					if (iColon >= 0) {
+						sClientName = sClientPort.left(iColon);
+						sPortName = sClientPort.right(sClientPort.length() - iColon - 1);
+					}
+				}
+				if (pClient && pClient->clientText() != sClientName)  {
+					pClient->setClientText(sClientName, false);
+					++iDirtyCount;
+				}
+				if (pPort && pPort->portText() != sPortName)  {
+					pPort->setPortText(sPortName, false);
+					++iDirtyCount;
+				}
+			#endif
 				if (pPort)
 					pPort->markClientPort(1);
 			}
@@ -388,10 +433,12 @@ int qjackctlJackClientList::updateClientPorts (void)
 
 	iDirtyCount += cleanClientPorts(0);
 
+#ifdef CONFIG_JACK_PORT_ALIASES
 	if (g_iJackClientPortAlias > 0) {
 		delete [] aliases[0];
 		delete [] aliases[1];
 	}
+#endif
 
 	return iDirtyCount;
 }

src/qjackctlConnect.cpp
@@ -1,7 +1,7 @@
 // qjackctlConnect.cpp
 //
 /****************************************************************************
-   Copyright (C) 2003-2022, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2003-2023, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License

src/qjackctlJackConnect.h
@@ -1,7 +1,7 @@
 // qjackctlJackConnect.h
 //
 /****************************************************************************
-   Copyright (C) 2003-2019, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2003-2023, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License

commit 2369891ea85835ca2b5fb36564d2dfae06067fec

Alias connection fixes #197
- Improve the alias lookup in the previous commit.

  This now looks for the aliases first (if enabled), to avoid an extra
  lookup of the real port name if aliases are enabled and available.

- Fix missing connections with aliases (#196).

  This fixes #196 by adjusting the call to findJackClientPort() in
  qjackctlJackConnect::updateConnections(), so that it also considers the
  aliases of the input ports if the target ports cannot be found by their
  real name.

  (This is done on the fly, using jack_port_get_aliases() on the spot. So
  this will incur a tiny bit of additional overhead, but only if the
  aliases display option is in effect.)

- Revert recent commits after rev. 49661937.

  This reverts the most recent commits 119cdf74..9b55062a which
  inadvertently broke the connections view with aliases in some cases.

src/qjackctlConnect.h
@@ -99,6 +99,8 @@ public:
 	// - Natural decimal sorting comparator.
 	bool operator< (const QTreeWidgetItem& other) const;
 
+protected:
+
 	// Port name display name accessors.
 	void setPortText(const QString& sPortText, bool bRenameEnabled);
 	QString portText() const;
@@ -171,6 +173,8 @@ public:
 	// Proxy sort override method.
 	// - Natural decimal sorting comparator.
 	bool operator< (const QTreeWidgetItem& other) const;
+
+protected:
 
 	// Client name display name accessors.
 	void setClientText(const QString& sClientText, bool bRenameEnabled);

src/qjackctlJackConnect.cpp
@@ -115,38 +115,12 @@ qjackctlJackPort::~qjackctlJackPort (void)
 // Pretty/display name method (virtual override).
 void qjackctlJackPort::updatePortName ( bool bRename )
 {
+#ifdef CONFIG_JACK_METADATA
 	jack_client_t *pJackClient = nullptr;
 	qjackctlMainForm *pMainForm = qjackctlMainForm::getInstance();
 	if (pMainForm)
 		pJackClient = pMainForm->jackClient();
-	if (pJackClient == nullptr)
-		return;
-
-#ifdef CONFIG_JACK_PORT_ALIASES
-	const int iJackClientPortAlias
-		= qjackctlJackClientList::jackClientPortAlias();
-	if (iJackClientPortAlias > 0) {
-		char *aliases[2];
-		const unsigned short alias_size = jack_port_name_size() + 1;
-		aliases[0] = new char [alias_size];
-		aliases[1] = new char [alias_size];
-		QString sPortName = portName();
-		QString sClientPort = clientName() + ':' + sPortName;
-		const QByteArray aClientPort = sClientPort.toUtf8();
-		const char *pszClientPort = aClientPort.constData();
-		jack_port_t *pJackPort = jack_port_by_name(pJackClient, pszClientPort);
-		if (pJackPort && jack_port_get_aliases(pJackPort, aliases) >= iJackClientPortAlias) {
-			sClientPort = QString::fromUtf8(aliases[iJackClientPortAlias - 1]);
-			sPortName = sClientPort.section(':', 1);
-		}
-		delete [] aliases[0];
-		delete [] aliases[1];
-		setPortText(sPortName, false);
-	}
-	else
-#endif
-#ifdef CONFIG_JACK_METADATA
-	if (qjackctlJackClientList::isJackClientPortMetadata()) {
+	if (pJackClient && qjackctlJackClientList::isJackClientPortMetadata()) {
 		bool bRenameEnabled = false;
 		QString sPortNameEx = portNameAlias(&bRenameEnabled);
 		const QString& sPortName = portName();
@@ -170,8 +144,8 @@ void qjackctlJackPort::updatePortName ( bool bRename )
 				&& !sPrettyName.isEmpty() && bRename) {
 				removePrettyName(pJackClient, port_uuid);
 			}
+			setPortText(sPortNameEx, bRenameEnabled);
 		}
-		setPortText(sPortNameEx, bRenameEnabled);
 	}
 	else
 #endif
@@ -257,15 +231,12 @@ qjackctlJackPort *qjackctlJackClient::findJackPort ( const QString& sPortName )
 // Pretty/display name method (virtual override).
 void qjackctlJackClient::updateClientName ( bool bRename )
 {
+#ifdef CONFIG_JACK_METADATA
 	jack_client_t *pJackClient = nullptr;
 	qjackctlMainForm *pMainForm = qjackctlMainForm::getInstance();
 	if (pMainForm)
 		pJackClient = pMainForm->jackClient();
-	if (pJackClient == nullptr)
-		return;
-
-#ifdef CONFIG_JACK_METADATA
-	if (qjackctlJackClientList::isJackClientPortMetadata()) {
+	if (pJackClient && qjackctlJackClientList::isJackClientPortMetadata()) {
 		bool bRenameEnabled = false;
 		QString sClientNameEx = clientNameAlias(&bRenameEnabled);
 		const QString& sClientName = clientName();
@@ -386,9 +357,14 @@ int qjackctlJackClientList::updateClientPorts (void)
 			QString sClientPort = QString::fromUtf8(ppszClientPorts[iClientPort]);
 			qjackctlJackClient *pClient = nullptr;
 			qjackctlJackPort *pPort = nullptr;
-			const char *pszClientPort = ppszClientPorts[iClientPort];
-			jack_port_t *pJackPort = jack_port_by_name(pJackClient, pszClientPort);
-			int iColon = sClientPort.indexOf(':');
+			jack_port_t *pJackPort = jack_port_by_name(pJackClient,
+				ppszClientPorts[iClientPort]);
+		#ifdef CONFIG_JACK_PORT_ALIASES
+			if (g_iJackClientPortAlias > 0 &&
+				jack_port_get_aliases(pJackPort, aliases) >= g_iJackClientPortAlias)
+				sClientPort = QString::fromUtf8(aliases[g_iJackClientPortAlias - 1]);
+		#endif
+			const int iColon = sClientPort.indexOf(':');
 			if (pJackPort && iColon >= 0) {
 				QString sClientName = sClientPort.left(iColon);
 				QString sPortName = sClientPort.right(sClientPort.length() - iColon - 1);
@@ -405,27 +381,6 @@ int qjackctlJackClientList::updateClientPorts (void)
 					pPort->setPortName(sPortName);
 					++iDirtyCount;
 				}
-			#ifdef CONFIG_JACK_PORT_ALIASES
-				if (g_iJackClientPortAlias > 0 &&
-					jack_port_get_aliases(pJackPort, aliases) >= g_iJackClientPortAlias) {
-					sClientPort = QString::fromUtf8(aliases[g_iJackClientPortAlias - 1]);
-					iColon = sClientPort.indexOf(':');
-					if (iColon >= 0) {
-						sClientName = sClientPort.left(iColon);
-						sPortName = sClientPort.right(sClientPort.length() - iColon - 1);
-					}
-				}
-				if (!g_bJackClientPortMetadata) {
-					if (pClient && pClient->clientText() != sClientName)  {
-						pClient->setClientText(sClientName, false);
-						++iDirtyCount;
-					}
-					if (pPort && pPort->portText() != sPortName)  {
-						pPort->setPortText(sPortName, false);
-						++iDirtyCount;
-					}
-				}
-			#endif
 				if (pPort)
 					pPort->markClientPort(1);
 			}
@@ -618,6 +573,9 @@ void qjackctlJackConnect::updateConnections (void)
 	if (pIClientList == nullptr)
 		return;
 
+#ifdef CONFIG_JACK_PORT_ALIASES
+	const int iJackClientPortAlias = qjackctlJackClientList::jackClientPortAlias();
+#endif
 	// For each output client item...
 	QListIterator<qjackctlClientItem *> oclient(OClientList()->clients());
 	while (oclient.hasNext()) {
@@ -646,9 +604,31 @@ void qjackctlJackConnect::updateConnections (void)
 					// Now, for each input client port...
 					for (int iClientPort = 0;
 							ppszClientPorts[iClientPort]; ++iClientPort) {
-						qjackctlPortItem *pIPort
-							= pIClientList->findJackClientPort(
+						qjackctlPortItem *pIPort = nullptr;
+					#ifdef CONFIG_JACK_PORT_ALIASES
+						if (iJackClientPortAlias > 0) {
+							// If Jack aliases are in effect, look for those if present -ag
+							jack_port_t *pIJackPort
+								= jack_port_by_name(pJackClient, ppszClientPorts[iClientPort]);
+							if (pIJackPort) {
+								char *aliases[2];
+								const unsigned short alias_size = jack_port_name_size() + 1;
+								aliases[0] = new char [alias_size];
+								aliases[1] = new char [alias_size];
+								if (jack_port_get_aliases(pIJackPort, aliases) >= iJackClientPortAlias) {
+									pIPort = pIClientList->findJackClientPort(aliases[iJackClientPortAlias - 1]);
+								}
+								delete [] aliases[0];
+								delete [] aliases[1];
+							}
+							// Otherwise we fall through to the default case below
+							// which looks for the port under its real name
+						}
+					#endif
+						if (!pIPort) {
+							pIPort = pIClientList->findJackClientPort(
 								ppszClientPorts[iClientPort]);
+						}
 						if (pIPort) {
 							pOPort->addConnect(pIPort);
 							pIPort->addConnect(pOPort);

src/translations/qjackctl_cs.ts
@@ -105,7 +105,7 @@
         <translation>Volba -n vyžaduje argument (název).</translation>
     </message>
     <message>
-        <location filename="../qjackctlJackConnect.cpp" line="204"/>
+        <location filename="../qjackctlJackConnect.cpp" line="178"/>
         <source>%1 (%2 frames)</source>
         <translation>%1 (%2 snímků)</translation>
     </message>

src/translations/qjackctl_de.ts
@@ -105,7 +105,7 @@
         <translation>Option -n benötigt ein Argument (Name).</translation>
     </message>
     <message>
-        <location filename="../qjackctlJackConnect.cpp" line="204"/>
+        <location filename="../qjackctlJackConnect.cpp" line="178"/>
         <source>%1 (%2 frames)</source>
         <translation type="unfinished"></translation>
     </message>

src/translations/qjackctl_es.ts
@@ -105,7 +105,7 @@
         <translation>La opción -n necesita un argumento (nombre).</translation>
     </message>
     <message>
-        <location filename="../qjackctlJackConnect.cpp" line="204"/>
+        <location filename="../qjackctlJackConnect.cpp" line="178"/>
         <source>%1 (%2 frames)</source>
         <translation>%1 (%2 cuadros)</translation>
     </message>

src/translations/qjackctl_fr.ts
@@ -105,7 +105,7 @@
         <translation>L&apos;option -n nécessite un argument (nom).</translation>
     </message>
     <message>
-        <location filename="../qjackctlJackConnect.cpp" line="204"/>
+        <location filename="../qjackctlJackConnect.cpp" line="178"/>
         <source>%1 (%2 frames)</source>
         <translation>%1 (%2 échantillons)</translation>
     </message>

src/translations/qjackctl_it.ts
@@ -105,7 +105,7 @@
         <translation>L&apos;opzione -n richiede un argomento (nome).</translation>
     </message>
     <message>
-        <location filename="../qjackctlJackConnect.cpp" line="204"/>
+        <location filename="../qjackctlJackConnect.cpp" line="178"/>
         <source>%1 (%2 frames)</source>
         <translation>%1 (%2 fotogrammi)</translation>
     </message>

src/translations/qjackctl_ja.ts
@@ -105,7 +105,7 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="../qjackctlJackConnect.cpp" line="204"/>
+        <location filename="../qjackctlJackConnect.cpp" line="178"/>
         <source>%1 (%2 frames)</source>
         <translation></translation>
     </message>

src/translations/qjackctl_ko.ts
@@ -105,7 +105,7 @@
         <translation>옵션 -n에는 인수(이름)가 필요합니다.</translation>
     </message>
     <message>
-        <location filename="../qjackctlJackConnect.cpp" line="204"/>
+        <location filename="../qjackctlJackConnect.cpp" line="178"/>
         <source>%1 (%2 frames)</source>
         <translation>%1 (%2 프레임)</translation>
     </message>

src/translations/qjackctl_nl.ts
@@ -105,7 +105,7 @@
         <translation>Optie -n vereist een argument (naam).</translation>
     </message>
     <message>
-        <location filename="../qjackctlJackConnect.cpp" line="204"/>
+        <location filename="../qjackctlJackConnect.cpp" line="178"/>
         <source>%1 (%2 frames)</source>
         <translation type="unfinished"></translation>
     </message>

src/translations/qjackctl_pt.ts
@@ -105,7 +105,7 @@
         <translation>Opção -n requer um argumento (nome).</translation>
     </message>
     <message>
-        <location filename="../qjackctlJackConnect.cpp" line="204"/>
+        <location filename="../qjackctlJackConnect.cpp" line="178"/>
         <source>%1 (%2 frames)</source>
         <translation>%1 (%2 quadros)</translation>
     </message>

src/translations/qjackctl_ru.ts
@@ -105,7 +105,7 @@
         <translation>Ключу -n необходим аргумент (название).</translation>
     </message>
     <message>
-        <location filename="../qjackctlJackConnect.cpp" line="204"/>
+        <location filename="../qjackctlJackConnect.cpp" line="178"/>
         <source>%1 (%2 frames)</source>
         <translation type="unfinished"></translation>
     </message>

src/translations/qjackctl_sk.ts
@@ -105,7 +105,7 @@
         <translation>Možnosť -n vyžaduje argument (názov).</translation>
     </message>
     <message>
-        <location filename="../qjackctlJackConnect.cpp" line="204"/>
+        <location filename="../qjackctlJackConnect.cpp" line="178"/>
         <source>%1 (%2 frames)</source>
         <translation>%1 (%2 snímkov)</translation>
     </message>

src/translations/qjackctl_uk.ts
@@ -37,7 +37,7 @@
         <translation>Перейменувати</translation>
     </message>
     <message>
-        <location filename="../qjackctlJackConnect.cpp" line="204"/>
+        <location filename="../qjackctlJackConnect.cpp" line="178"/>
         <source>%1 (%2 frames)</source>
         <translation>%1 (%2 кадрів)</translation>
     </message>