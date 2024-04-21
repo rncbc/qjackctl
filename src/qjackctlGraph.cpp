68346b7b407908d7b65ad3c96b682d9daa884b8c
rncbc <rncbc@rncbc.org>
2024-04-21 11:35:41 +0100

    Garaph: give thumbview size 1px slack (for no-gaps on the bottom and right edges).

--- src/qjackctlGraph.cpp
+++ src/qjackctlGraph.cpp
@@ -3008,7 +3008,7 @@ void qjackctlGraphThumb::updatePosition (void)
 	const QRect& rect = m_canvas->viewport()->rect();
 	const int w = rect.width()  / 4;
 	const int h = rect.height() / 4;
-	QFrame:setFixedSize(w, h);
+	QFrame:setFixedSize(w + 1, h + 1); // 1px slack.
 
 	switch (m_position) {
 	case TopLeft:
