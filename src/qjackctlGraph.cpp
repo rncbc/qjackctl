d44956c5402d7e5c2a53664254ab612895ac0492
rncbc <rncbc@rncbc.org>
2026-04-27 16:04:53 +0100

    - Graph: Make sure thumb-view widget background is filled/opaque (not transparent)

--- src/qjackctlGraph.cpp
+++ src/qjackctlGraph.cpp
@@ -1,7 +1,7 @@
 // qjackctlGraph.cpp
 //
 /****************************************************************************
-   Copyright (C) 2003-2025, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2003-2026, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -2969,6 +2969,7 @@ public:
 
 		QGraphicsView::setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 		QGraphicsView::setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+		QGraphicsView::setAutoFillBackground(true);
 
 		updatePalette();
 
